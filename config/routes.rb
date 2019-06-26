0f6e3f3275409eabbefeabfa6a21f49e92004638
Tiago Sa <tiagosousasa@gmail.com>
2019-06-26 19:13:48 -0400

    ClearanceAuth Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'posts#index', as: 'home' #this is the root index where local host first loads
   get 'about' => 'pages#about', as: 'about'
   resources :posts do 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_14_224657) do
+ActiveRecord::Schema.define(version: 2019_06_26_193445) do
 
   create_table "comments", force: :cascade do |t|
     t.string "username"
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 2019_06_14_224657) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
